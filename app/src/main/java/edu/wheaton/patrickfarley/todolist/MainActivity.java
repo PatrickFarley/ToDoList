73b2696a0880a975092d911f27fce2451023a71a
patrickfarley <patrick.farley@my.wheaton.edu>
2015-06-24 16:43:19 -0700

    db updating is now done thru alert dialogs when a task is clicked (instead of on enter key)

--- app/src/main/java/edu/wheaton/patrickfarley/todolist/MainActivity.java
+++ app/src/main/java/edu/wheaton/patrickfarley/todolist/MainActivity.java
@@ -154,15 +154,14 @@ public class MainActivity extends ListActivity {
      *
      */
     private void updateUI() {
-        Log.i("updateUI","updateUI called");
+        Log.i("updateUI", "updateUI called");
         // open the database and get a cursor over the desired fields
         helper = new TaskDBHelper(MainActivity.this);
         SQLiteDatabase sqlDB = helper.getWritableDatabase();
         Cursor cursor = sqlDB.query(TaskContract.TABLE,
                 new String[]{TaskContract.Columns._ID, TaskContract.Columns.TASK,
-                TaskContract.Columns.PRIORITY, TaskContract.Columns.TIME},
-                null,null,null,null,null);
-
+                        TaskContract.Columns.PRIORITY, TaskContract.Columns.TIME},
+                null, null, null, null, null);
 
 
         // construct this activity's ListAdapter: we must pass in the task_view layout, as well as the location
@@ -171,10 +170,10 @@ public class MainActivity extends ListActivity {
                 this, // context
                 R.layout.task_view, // the layout template to use to present each data
                 cursor, // pass in the cursor to bind to.
-                new String[] {TaskContract.Columns.TASK,
+                new String[]{TaskContract.Columns.TASK,
                         TaskContract.Columns.PRIORITY,
                         TaskContract.Columns.TIME}, // array of items to bind
-                new int[] {R.id.taskField,
+                new int[]{R.id.taskField,
                         R.id.priorityField,
                         R.id.timeField},// parallel corresponding array of layout object to bind data to
                 0) {
@@ -182,85 +181,18 @@ public class MainActivity extends ListActivity {
             // We override the getView method so that we can make changes to all task_views and their
             // child views, as they are being created/filled by the SimpleCursorAdapter.
             @Override
-            public View getView (int position, View convertView, ViewGroup parent) {
+            public View getView(int position, View convertView, ViewGroup parent) {
                 View view = super.getView(position, convertView, parent);
 
                 // we tag the view with its database row id
-                view.setTag(getItemId(position));
+                long viewId = getItemId(position);
 
-                // entryUpdateListener is assigned to the time and priority views below
-                View.OnKeyListener entryUpdateListener = new View.OnKeyListener() {
+                // editAlertListener is assigned to the time and priority views below
+                View.OnClickListener myEditAlertListener = new EditAlertListener(viewId) ;
 
-                    @Override
-                    public boolean onKey(View v, int keyCode, KeyEvent event){
-                        if (event.getAction() == KeyEvent.ACTION_DOWN){
-                            if (keyCode == KeyEvent.KEYCODE_ENTER) {
-                                Log.d("in Main", "key down enter pressed");
-
-                                // Hide the keyboard from view:
-                                InputMethodManager in = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                                in.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-
-                                // get a ref to the parent of the calling view (will be a task_view)
-                                View parent = (View) v.getParent();
-
-                                // get refs to the views within the parent
-                                EditText priorityField = (EditText) parent.findViewById(R.id.priorityField);
-                                EditText timeField  = (EditText) parent.findViewById(R.id.timeField);
-
-                                // get a ref to the id tag of the parent
-                                long id = (long)parent.getTag();
-
-                                int priority;
-                                int time;
-
-                                try {
-                                    // get the info from the views:
-                                    priority = Integer.parseInt(priorityField.getText().toString());
-                                    time = Integer.parseInt(timeField.getText().toString());
-                                } catch (NumberFormatException e) {
-                                    Context context = getApplicationContext();
-                                    CharSequence toastMsg = "Enter a positive integer for priority and time";
-                                    int duration = Toast.LENGTH_SHORT;
-                                    Toast toast = Toast.makeText(context, toastMsg, duration);
-                                    toast.show();
-                                    return true;
-                                }
-
-                                Log.i("entryUpdateListener","priority= "+priority+" and time= "+time);
-
-                                // Now write an SQL command to update said row on the table by matching its _id value
-                                String sql = String.format("UPDATE %s SET %s = %d , %s = %d WHERE %s = %d",
-                                        TaskContract.TABLE,
-                                        TaskContract.Columns.PRIORITY,
-                                        priority,
-                                        TaskContract.Columns.TIME,
-                                        time,
-                                        TaskContract.Columns._ID,
-                                        id);
-
-                                // Construct a DB helper, and use it to return a reference
-                                // to MainActivity's database
-                                helper = new TaskDBHelper(MainActivity.this);
-                                SQLiteDatabase sqlDB = helper.getWritableDatabase();
-
-                                Log.d("SimpleCursorAdapter","command is "+sql);
-
-                                // execute the command in the database
-                                sqlDB.execSQL(sql);
-                                return true;
-                            }
-                        }
-                        return false;
-                    }
-                };
-
-                EditText priorityField = (EditText) view.findViewById(R.id.priorityField);
-                priorityField.setOnKeyListener(entryUpdateListener);
-
-
-                EditText timeField = (EditText) view.findViewById(R.id.timeField);
-                timeField.setOnKeyListener(entryUpdateListener);
+                view.findViewById(R.id.taskField).setOnClickListener(myEditAlertListener);
+                view.findViewById(R.id.priorityField).setOnClickListener(myEditAlertListener);
+                view.findViewById(R.id.timeField).setOnClickListener(myEditAlertListener);
 
                 // getView method must return the original view (the task_view)
                 return view;
@@ -270,9 +202,8 @@ public class MainActivity extends ListActivity {
         // MainActivity displays a ListView and uses a ListAdapter to bind the data to the view.
         // We pass in the taskListAdapter, defined above
         this.setListAdapter(taskListAdapter);
+
     }
-
-
 
 
 
@@ -286,9 +217,9 @@ public class MainActivity extends ListActivity {
      */
     public void onDoneButtonClick(View view) {
         // get a ref to the parent view of the calling button (get the listView)
-        View v = (View) view.getParent();
+        View parent = (View) view.getParent();
         // get a ref to the TextView corresponding to this button
-        TextView taskField = (TextView) v.findViewById(R.id.taskField);
+        TextView taskField = (TextView) parent.findViewById(R.id.taskField);
         // get the text from that TextView
         String task = taskField.getText().toString();
 
@@ -333,5 +264,111 @@ public class MainActivity extends ListActivity {
             return;
         }
 
+    }
+
+
+    private class EditAlertListener implements View.OnClickListener {
+
+        long viewId;
+
+        public EditAlertListener(long viewId){
+            this.viewId = viewId;
+        }
+
+        public void onClick(View v) {
+
+            View parent =(View) v.getParent();
+        Log.d("in Main", "an EditText is clicked");
+
+
+        // use an AlertDialog Builder object
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("edit item");
+
+        // construct a View with the new_task_entry layout
+        final LayoutInflater inflater = getLayoutInflater();
+        final View startView = inflater.inflate(R.layout.new_task_entry, null);
+
+
+        // Fill in the existing information for this task:
+
+        TextView taskField = (TextView) parent.findViewById(R.id.taskField);
+        TextView priorityField = (TextView) parent.findViewById(R.id.priorityField);
+        TextView timeField = (TextView) parent.findViewById(R.id.timeField);
+
+        String oldName = taskField.getText().toString();
+        String oldPriority = priorityField.getText().toString();
+        String oldTime = timeField.getText().toString();
+
+        EditText startName = (EditText) startView.findViewById(R.id.taskEntry);
+        EditText startPriority = (EditText) startView.findViewById(R.id.priorityEntry);
+        EditText startTime = (EditText) startView.findViewById(R.id.timeEntry);
+
+        startName.setText(oldName);
+        startPriority.setText(oldPriority);
+        startTime.setText(oldTime);
+
+
+        // pass the view into the alertDialog, for the user to ineract with
+        builder.setView(startView);
+
+        // set "enter" button action
+        builder.setPositiveButton("enter", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                // store entered data:
+                final EditText taskField = (EditText) startView.findViewById(R.id.taskEntry);
+                final EditText priorityField = (EditText) startView.findViewById(R.id.priorityEntry);
+                final EditText timeField = (EditText) startView.findViewById(R.id.timeEntry);
+
+                String task;
+                int priority;
+                int time;
+
+                try {
+                    task = taskField.getText().toString();
+                    priority = Integer.parseInt(priorityField.getText().toString());
+                    time = Integer.parseInt(timeField.getText().toString());
+                } catch (NumberFormatException e) {
+                    Context context = getApplicationContext();
+                    CharSequence toastMsg = "Enter a positive integer for priority and time";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, toastMsg, duration);
+                    toast.show();
+                    return;
+                }
+
+                // construct a TaskDBHelper object here
+                helper = new TaskDBHelper(MainActivity.this);
+
+                // reference the database: this automatically references MainActivity's
+                // database
+                SQLiteDatabase db = helper.getWritableDatabase();
+
+                // make a ContentValues object
+                ContentValues values = new ContentValues();
+                values.clear();
+
+                // put our info into the appropriate columns of the ContentValues object
+                values.put(TaskContract.Columns.TASK, task);
+                values.put(TaskContract.Columns.PRIORITY, priority);
+                values.put(TaskContract.Columns.TIME, time);
+
+                // insert the columns from the ContentValues object into the referenced
+                // database table.
+                db.update(TaskContract.TABLE, values, TaskContract.Columns._ID + " = " + viewId, null);
+
+                // update the UI from the database
+
+                updateUI();
+            }
+        });
+            // set "Cancel" button action
+            builder.setNegativeButton("Cancel",null);
+            // create and display the Alert Dialog
+            builder.create().show();
+        }
     }
 }
